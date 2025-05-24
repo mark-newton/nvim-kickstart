cf52737b34b7042f8e63a519db74fac4f9180e70
Mark Newton <marco@twistedpair.com.au>
2025-05-24 11:27:53 +1000

    mini and textcase plugins

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,7 +1,8 @@
 return {
   'echasnovski/mini.nvim',
   config = function()
-    -- NOTE: ai & surround already included in kickstart
+    require('mini.ai').setup { n_lines = 500 }
+    require('mini.surround').setup()
 
     require('mini.indentscope').setup {
       symbol = '│',

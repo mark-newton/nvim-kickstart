5816f598ef43e480a720f67ec594e904a25032ed
Mark Newton <marco@twistedpair.com.au>
2025-06-05 15:38:35 +1000

    add mini splitjoin. add comments for mini surround (i keep forgetting them)

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -3,6 +3,10 @@ return {
   config = function()
     require('mini.ai').setup { n_lines = 500 }
     require('mini.surround').setup()
+    -- basic default mappings for surround:
+    -- sa'    add quotes (visual mode)
+    -- sr'"   replace ' with " (normal mode cursor)
+    -- sd'    delete ' (normal mode cursor)
 
     require('mini.indentscope').setup {
       symbol = '│',
@@ -17,6 +21,12 @@ return {
         go_out = '<left>',
         go_in_plus = '<CR>',
         reveal_cwd = '.',
+      },
+    }
+
+    require('mini.splitjoin').setup {
+      mappings = {
+        toggle = '<leader>j',
       },
     }
 
